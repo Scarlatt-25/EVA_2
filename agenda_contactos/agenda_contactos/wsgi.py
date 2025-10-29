4500a997dd0e572c6eafbe612f005465ab92928e
Scarlatt-25 <amara.duque@inacapmail.cl>
2025-10-29 16:15:30 -0300

    15

--- agenda_contactos/agenda_contactos/wsgi.py
+++ agenda_contactos/agenda_contactos/wsgi.py
@@ -7,10 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+# /app/agenda_contactos/wsgi.py
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agenda_contactos.settings')
-
-
 application = get_wsgi_application()
