e9211385ec63e2cf3850a7330b8a5eb7c52f4697
Scarlatt-25 <amara.duque@inacapmail.cl>
2025-10-29 19:39:16 -0300

    17

--- agenda_contactos/contactos/apps.py
+++ agenda_contactos/contactos/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
-
+import os
+from django.core.management import execute_from_command_line
 
 class ContactosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contactos'
+
+
+port = os.environ.get("PORT", "8000")  # 8000 de fallback
+execute_from_command_line(["manage.py", "runserver", f"0.0.0.0:{port}"])
